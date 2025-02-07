a4a8b5f7139653dacc4292e8b6e80ec9bb0ef11e
kenneth.liu <kenneth.liu@sophgo.com>
2025-02-07 17:04:14 +0800

    bsp: cvitek: add read/write reg cmd for C920_big
    1. add rt_mr/rt_mw cmd for read/write reg

---
+++ bsp/cvitek/c920_big/applications/reg_test.c
@@ -0,0 +1,61 @@
+#include <rtthread.h>
+#include "mmio.h"
+#include "stdlib.h"
+
+static void rt_mr(int argc, char**argv)
+{
+    if(argc != 2)
+    {
+        rt_kprintf("rt_mr <addr>\n");
+        return;
+    }
+    uint64_t addr = strtol(argv[1], NULL, 16);
+    uint32_t reg = mmio_read_32(addr);
+    rt_kprintf("read <0x%lx>: 0x%x\n", addr, reg);
+}
+
+MSH_CMD_EXPORT(rt_mr, mmio read reg: rt_mr <addr>);
+
+static void rt_mr64(int argc, char**argv)
+{
+    if(argc != 2)
+    {
+        rt_kprintf("rt_mr64 <addr>\n");
+        return;
+    }
+    uint64_t addr = strtol(argv[1], NULL, 16);
+    uint64_t reg = mmio_read_64(addr);
+    rt_kprintf("read <0x%lx>: 0x%lx\n", addr, reg);
+}
+
+MSH_CMD_EXPORT(rt_mr64, mmio read reg: rt_mr64 <addr>);
+
+static void rt_mw64(int argc, char**argv)
+{
+    if(argc != 3)
+    {
+        rt_kprintf("rt_mw64 <addr> <val>\n");
+        return;
+    }
+    uint64_t addr = strtol(argv[1], NULL, 16);
+    uint64_t reg = strtol(argv[2], NULL, 16);
+    mmio_write_64(addr, reg);
+    rt_kprintf("write <0x%lx>: 0x%lx\n", addr, reg);
+}
+
+MSH_CMD_EXPORT(rt_mw64, mmio write reg: rt_mw64 <addr>);
+
+static void rt_mw(int argc, char**argv)
+{
+    if(argc != 3)
+    {
+        rt_kprintf("rt_mw <addr> <val>\n");
+        return;
+    }
+    uint64_t addr = strtol(argv[1], NULL, 16);
+    uint32_t reg = strtol(argv[2], NULL, 16);
+    mmio_write_32(addr, reg);
+    rt_kprintf("write <0x%lx>: 0x%x\n", addr, reg);
+}
+
+MSH_CMD_EXPORT(rt_mw, mmio write reg: rt_mw <addr>);
